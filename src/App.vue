c8ca1b7a56b49b54a8f893df4d6a8826a02d8f64
joemcgee4151986 <joerobertmcgee@gmail.com>
2022-12-26 16:26:10 +0900

    restarted the project and added new code

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class = "container">
+      
+   <img class = scarecrow src ="./assets/Scarecrow.png"
+  max-height="10">
+    
+    <h1>I have bad news for you!</h1>
+    <p>The page you are looking for might be removed or is temporarily unavailable</p> 
+  </div>
+  <main>
+   
+  </main>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap')
+
+
+body {
+        font-family: "Poppins", sans-serif;
+        background-color: white;
+      }
+
+      ul {
+        padding-left: 24px;
+      }
+
+      li {
+        margin-bottom: 8px;
+      }
+
+      .welcome {
+        max-width: 800px;
+        margin: 0 auto;
+      }
+
+      .welcome-text {
+        margin-top: 30vh;
+        font-weight: 500;
+      }
+
+      .welcome-text p {
+        font-size: 1rem;
+        font-weight: 400;
+      }
+
+      .welcome-text h1 {
+        font-size: 1.5rem;
+      }
+
+      .welcome-text a {
+        color: #f7542e;
+      }
+ 
+</style>
