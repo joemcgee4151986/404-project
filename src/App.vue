cdbe7fe624c7b729a8b477cdde7f4c0152fdb3cb
joemcgee4151986 <joerobertmcgee@gmail.com>
2022-12-27 13:55:06 +0900

    updated the design

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
+
+
 <script setup>
 
 </script>
@@ -10,6 +12,7 @@
     
     <h1>I have bad news for you!</h1>
     <p>The page you are looking for might be removed or is temporarily unavailable</p> 
+    <button>Back to Homepage</button>
   </div>
   <main>
    
